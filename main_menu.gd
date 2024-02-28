471d3fdb5b969c9521f4866286e26b08af245c0b
JohnRossDerez <jtderez@up.edu.ph>
2024-02-28 15:18:19 +0800

    Added scripts in GD

---
+++ main_menu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_new_button_pressed():
+	get_tree().change_scene_to_file("res://start_ui.tscn")
+
+
+func _on_gloss_button_pressed():
+	get_tree().change_scene_to_file("res://glossary_ui.tscn")
+
+
+func _on_load_button_pressed():
+	get_tree().change_scene_to_file("res://start_ui.tscn")
