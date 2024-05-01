d1295c26933ee97c7e8b7708c8f4960afae3f7f9
JohnRossDerez <jtderez@up.edu.ph>
2024-05-01 20:32:35 +0800

    Branch minigame

---
+++ Scenes/Minigame/dino.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 1300
+const JUMP_SPEED : int = -800
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		#$RunCol.disabled = false
+		if Input.is_action_pressed("ui_accept"):
+			velocity.y = JUMP_SPEED
+			#$JumpSound.play()
+		elif Input.is_action_pressed("ui_down"):
+			$AnimatedSprite2D.play("duck")
+			#$RunCol.disabled = true
+		else:
+			$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+		
+	move_and_slide()
